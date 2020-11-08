bf2ed3528d3603e12df3fc84d891ece089566973
石浩冰 <2662830896@qq.com>
2020-11-08 11:52:49 +0800

    1

--- 石浩冰/test/py.py
+++ 石浩冰/test/py.py
@@ -1,1 +1,22 @@
-print (1)
+yr=int(input('请输入年份：'))
+if yr%400==0:
+    print("闰年")
+else:
+    print('平年')
+
+
+for a in range(1,10):
+    for b in range(10):
+        for c in range(10):
+            sxh=(a*100+b*10+c)
+            if sxh==(a**3+b**3+c**3):
+                print(sxh)
+
+
+import math
+a=float(input('请输入a:'))
+b=float(input('请输入b:'))
+c=float(input('请输入c:'))
+x1=((-b+math.sqrt(b**2-4*a*c))/(2*a))
+x2=((-b-math.sqrt(b**2-4*a*c))/(2*a))
+print('x1=',x1,'\t','x2',x2)
